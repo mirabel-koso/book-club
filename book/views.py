fc0f789f5d477dc2fd2eb46445a8e89380f08da9
Mirabel Ekwenugo <mirabel.ekwenugo@andela.com>
2016-09-19 13:36:42 +0100

    add index template and materialize css to book app

--- book/views.py
+++ book/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.db.models import Q
+
+from models import Book, Category
 
 # Create your views here.
 
 
 class ListAllBooks(TemplateView):
+
+    template_name = 'index.html'
 
     @staticmethod
     def filter_search(search_query, context):
