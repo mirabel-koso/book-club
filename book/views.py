69ea00856e185fb44e29e5bd886cf933b7e8bb0e
Mirabel Ekwenugo <mirabel.ekwenugo@andela.com>
2016-09-19 13:20:16 +0100

    add home view url

--- book/views.py
+++ book/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
 # Create your views here.
+
+
+class ListAllBooks(TemplateView):
+
+    @staticmethod
+    def filter_search(search_query, context):
+        pass
--- book_club/urls.py
+++ book_club/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from book import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.ListAllBooks.as_view(), name='home_view'),
 ]
